5bb0407c5084507f5e08610eb28123da6c5fa121
Hendrik Kurniawan <hendrik.kurniawan@rtl.nl>
2016-08-07 14:50:25 +0200

    adds bodyparser

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+
 app.set('port', process.env.PORT || 8080);
+
+app.use(bodyParser.json());
 
 function receivedMessage(event) {
   var senderID = event.sender.id;
